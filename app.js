ff29bb0c0ff969f7520dad6276b5dc6a2d508310
22avinash <arai08230@gmail.com>
2015-10-09 22:03:53 +0530

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,7 +13,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+
+app.engine('html',require('hbs').__express);
+app.set('view engine','html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/register',require('./routes/register'));
+
+var user = require('./models/user');
+
+app.get('/list',function(req,res){
+  user.find(function(err,userlist){
+    res.send(userlist);
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,6 +58,7 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
+  mongoose.connect("mongodb://localhost/MyApp");
 }
 
 // production error handler
